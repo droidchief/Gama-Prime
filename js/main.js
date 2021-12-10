0804cfca2decb143b68d0b813d2148e4c7a7d6c7
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2021-12-10 04:30:32 +0100

    Fix: boostrap files

--- js/main.js
+++ js/main.js
@@ -1,39 +1,39 @@
-$(function() {
-    $("#wizard").steps({
+$(function(){
+	$("#wizard").steps({
         headerTag: "h2",
         bodyTag: "section",
         transitionEffect: "fade",
         enableAllSteps: true,
         transitionEffectSpeed: 500,
         labels: {
-            finish: "Sign In",
-            next: "Next",
-            previous: "Previous"
+            finish: "Submit",
+            next: "Forward",
+            previous: "Backward"
         }
     });
-    $('.wizard > .steps li a').click(function() {
-        $(this).parent().addClass('checked');
-        $(this).parent().prevAll().addClass('checked');
-        $(this).parent().nextAll().removeClass('checked');
+    $('.wizard > .steps li a').click(function(){
+    	$(this).parent().addClass('checked');
+		$(this).parent().prevAll().addClass('checked');
+		$(this).parent().nextAll().removeClass('checked');
     });
     // Custome Jquery Step Button
-    $('.forward').click(function() {
-        $("#wizard").steps('next');
+    $('.forward').click(function(){
+    	$("#wizard").steps('next');
     })
-    $('.backward').click(function() {
-            $("#wizard").steps('previous');
-        })
-        // Select Dropdown
+    $('.backward').click(function(){
+        $("#wizard").steps('previous');
+    })
+    // Select Dropdown
     $('html').click(function() {
-        $('.select .dropdown').hide();
+        $('.select .dropdown').hide(); 
     });
-    $('.select').click(function(event) {
+    $('.select').click(function(event){
         event.stopPropagation();
     });
-    $('.select .select-control').click(function() {
+    $('.select .select-control').click(function(){
         $(this).parent().next().toggle();
-    })
-    $('.select .dropdown li').click(function() {
+    })    
+    $('.select .dropdown li').click(function(){
         $(this).parent().toggle();
         var text = $(this).attr('rel');
         $(this).parent().prev().find('div').text(text);
